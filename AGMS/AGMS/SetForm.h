c54c552846a026db5c976b79999babea0d97daa7
Dexter <39230159+Dexter-Zhong@users.noreply.github.com>
2018-12-07 15:28:16 +0800

    V4.1

--- AGMS/AGMS/SetForm.h
+++ AGMS/AGMS/SetForm.h
@@ -120,10 +120,9 @@ namespace AGMS {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"宋体", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(134)));
-			this->label1->Location = System::Drawing::Point(172, 28);
-			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Location = System::Drawing::Point(129, 22);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(187, 25);
+			this->label1->Size = System::Drawing::Size(149, 20);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"环境合理值设置";
 			// 
@@ -132,10 +131,9 @@ namespace AGMS {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(134)));
-			this->label2->Location = System::Drawing::Point(22, 97);
-			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Location = System::Drawing::Point(16, 78);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(79, 20);
+			this->label2->Size = System::Drawing::Size(64, 16);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"开启PID";
 			// 
@@ -144,10 +142,9 @@ namespace AGMS {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(134)));
-			this->label3->Location = System::Drawing::Point(37, 151);
-			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label3->Location = System::Drawing::Point(28, 121);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(49, 20);
+			this->label3->Size = System::Drawing::Size(40, 16);
 			this->label3->TabIndex = 1;
 			this->label3->Text = L"温度";
 			// 
@@ -156,10 +153,9 @@ namespace AGMS {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(134)));
-			this->label4->Location = System::Drawing::Point(37, 205);
-			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label4->Location = System::Drawing::Point(28, 164);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(49, 20);
+			this->label4->Size = System::Drawing::Size(40, 16);
 			this->label4->TabIndex = 1;
 			this->label4->Text = L"湿度";
 			// 
@@ -168,58 +164,52 @@ namespace AGMS {
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"宋体", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(134)));
-			this->label5->Location = System::Drawing::Point(13, 259);
-			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label5->Location = System::Drawing::Point(10, 207);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(109, 20);
+			this->label5->Size = System::Drawing::Size(88, 16);
 			this->label5->TabIndex = 1;
 			this->label5->Text = L"氟化物浓度";
 			// 
 			// numericUpDown1
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(124, 155);
-			this->numericUpDown1->Margin = System::Windows::Forms::Padding(4);
+			this->numericUpDown1->Location = System::Drawing::Point(93, 124);
 			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
 			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown1->Name = L"numericUpDown1";
-			this->numericUpDown1->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown1->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown1->TabIndex = 2;
 			// 
 			// numericUpDown3
 			// 
-			this->numericUpDown3->Location = System::Drawing::Point(124, 99);
-			this->numericUpDown3->Margin = System::Windows::Forms::Padding(4);
+			this->numericUpDown3->Location = System::Drawing::Point(93, 79);
 			this->numericUpDown3->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
 			this->numericUpDown3->Name = L"numericUpDown3";
-			this->numericUpDown3->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown3->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown3->TabIndex = 2;
 			// 
 			// numericUpDown5
 			// 
-			this->numericUpDown5->Location = System::Drawing::Point(124, 202);
-			this->numericUpDown5->Margin = System::Windows::Forms::Padding(4);
+			this->numericUpDown5->Location = System::Drawing::Point(93, 162);
 			this->numericUpDown5->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
 			this->numericUpDown5->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown5->Name = L"numericUpDown5";
-			this->numericUpDown5->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown5->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown5->TabIndex = 2;
 			// 
 			// numericUpDown7
 			// 
-			this->numericUpDown7->Location = System::Drawing::Point(124, 256);
-			this->numericUpDown7->Margin = System::Windows::Forms::Padding(4);
+			this->numericUpDown7->Location = System::Drawing::Point(93, 205);
 			this->numericUpDown7->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
 			this->numericUpDown7->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown7->Name = L"numericUpDown7";
-			this->numericUpDown7->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown7->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown7->TabIndex = 2;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(124, 325);
-			this->button1->Margin = System::Windows::Forms::Padding(4);
+			this->button1->Location = System::Drawing::Point(93, 260);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(95, 34);
+			this->button1->Size = System::Drawing::Size(71, 27);
 			this->button1->TabIndex = 7;
 			this->button1->Text = L"确定";
 			this->button1->UseVisualStyleBackColor = true;
@@ -227,10 +217,9 @@ namespace AGMS {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(319, 325);
-			this->button2->Margin = System::Windows::Forms::Padding(4);
+			this->button2->Location = System::Drawing::Point(239, 260);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(95, 34);
+			this->button2->Size = System::Drawing::Size(71, 27);
 			this->button2->TabIndex = 7;
 			this->button2->Text = L"退出";
 			this->button2->UseVisualStyleBackColor = true;
@@ -239,108 +228,111 @@ namespace AGMS {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(142, 64);
+			this->label6->Location = System::Drawing::Point(106, 51);
+			this->label6->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(45, 15);
+			this->label6->Size = System::Drawing::Size(35, 12);
 			this->label6->TabIndex = 8;
 			this->label6->Text = L"1号车";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(289, 64);
+			this->label7->Location = System::Drawing::Point(217, 51);
+			this->label7->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(45, 15);
+			this->label7->Size = System::Drawing::Size(35, 12);
 			this->label7->TabIndex = 9;
 			this->label7->Text = L"2号车";
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(430, 64);
+			this->label8->Location = System::Drawing::Point(322, 51);
+			this->label8->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(45, 15);
+			this->label8->Size = System::Drawing::Size(35, 12);
 			this->label8->TabIndex = 10;
 			this->label8->Text = L"3号车";
 			// 
 			// numericUpDown2
 			// 
-			this->numericUpDown2->Location = System::Drawing::Point(264, 200);
-			this->numericUpDown2->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown2->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown2->Location = System::Drawing::Point(198, 160);
+			this->numericUpDown2->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown2->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown2->Name = L"numericUpDown2";
-			this->numericUpDown2->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown2->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown2->TabIndex = 11;
 			// 
 			// numericUpDown4
 			// 
-			this->numericUpDown4->Location = System::Drawing::Point(264, 151);
-			this->numericUpDown4->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown4->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown4->Location = System::Drawing::Point(198, 121);
+			this->numericUpDown4->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown4->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown4->Name = L"numericUpDown4";
-			this->numericUpDown4->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown4->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown4->TabIndex = 12;
 			// 
 			// numericUpDown6
 			// 
-			this->numericUpDown6->Location = System::Drawing::Point(264, 254);
-			this->numericUpDown6->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown6->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown6->Location = System::Drawing::Point(198, 203);
+			this->numericUpDown6->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown6->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown6->Name = L"numericUpDown6";
-			this->numericUpDown6->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown6->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown6->TabIndex = 13;
 			// 
 			// numericUpDown8
 			// 
-			this->numericUpDown8->Location = System::Drawing::Point(264, 99);
-			this->numericUpDown8->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown8->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown8->Location = System::Drawing::Point(198, 79);
+			this->numericUpDown8->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown8->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown8->Name = L"numericUpDown8";
-			this->numericUpDown8->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown8->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown8->TabIndex = 14;
 			// 
 			// numericUpDown9
 			// 
-			this->numericUpDown9->Location = System::Drawing::Point(402, 256);
-			this->numericUpDown9->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown9->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown9->Location = System::Drawing::Point(302, 205);
+			this->numericUpDown9->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown9->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown9->Name = L"numericUpDown9";
-			this->numericUpDown9->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown9->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown9->TabIndex = 15;
 			// 
 			// numericUpDown10
 			// 
-			this->numericUpDown10->Location = System::Drawing::Point(402, 205);
-			this->numericUpDown10->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown10->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown10->Location = System::Drawing::Point(302, 164);
+			this->numericUpDown10->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown10->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown10->Name = L"numericUpDown10";
-			this->numericUpDown10->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown10->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown10->TabIndex = 16;
 			// 
 			// numericUpDown11
 			// 
-			this->numericUpDown11->Location = System::Drawing::Point(402, 151);
-			this->numericUpDown11->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown11->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown11->Location = System::Drawing::Point(302, 121);
+			this->numericUpDown11->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown11->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown11->Name = L"numericUpDown11";
-			this->numericUpDown11->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown11->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown11->TabIndex = 17;
 			// 
 			// numericUpDown12
 			// 
-			this->numericUpDown12->Location = System::Drawing::Point(402, 97);
-			this->numericUpDown12->Margin = System::Windows::Forms::Padding(4);
-			this->numericUpDown12->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numericUpDown12->Location = System::Drawing::Point(302, 78);
+			this->numericUpDown12->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->numericUpDown12->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, System::Int32::MinValue });
 			this->numericUpDown12->Name = L"numericUpDown12";
-			this->numericUpDown12->Size = System::Drawing::Size(95, 25);
+			this->numericUpDown12->Size = System::Drawing::Size(71, 21);
 			this->numericUpDown12->TabIndex = 18;
 			// 
 			// SetForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::MenuBar;
-			this->ClientSize = System::Drawing::Size(523, 411);
+			this->ClientSize = System::Drawing::Size(392, 329);
 			this->Controls->Add(this->numericUpDown12);
 			this->Controls->Add(this->numericUpDown11);
 			this->Controls->Add(this->numericUpDown10);
@@ -363,7 +355,6 @@ namespace AGMS {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"SetForm";
 			this->Text = L"期望环境设置";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
